70f529ac375253d320b8c89ee2a287de81402c08
Noah Roerig <noahr1936@gmail.com>
2023-01-13 13:54:21 -0700

    Profile change

--- src/settings/Profile.tsx
+++ src/settings/Profile.tsx
@@ -2,16 +2,35 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React, { useEffect, useState } from "react";
 import { Formik } from "formik";
 
+const dummyProfileData = {
+    firstName: "Noah", 
+    lastName: "Roerig",
+    email: "NRoerig@my.gcu.edu",
+    gender: "MALE",
+    language: "ENGLISH",
+    dob: "02/21/2001",
+    graduationTerm: "Spring 2023",
+    visibility: "PRIVATE"
+}
+
 export default function Profile() {
     const { user, getAccessTokenSilently } = useAuth0();
     const [token, setToken] = useState<string>();
+    const [profile, setProfile] = useState(dummyProfileData);
 
     useEffect(() => {
+        console.log("Profile Fetch");
+        
         const getToken = async () => {
             setToken(await getAccessTokenSilently());
         };
 
+        const getProfileInfo = async() => {
+            //todo: perform api request to fetch user data
+        }
+
         getToken();
+        getProfileInfo();
     });
 
     const editProfile = () => {        
@@ -27,6 +46,8 @@ export default function Profile() {
                 <u>Profile</u>
             </h5>
             <Formik
+                initialValues={profile}
+                validationSchema={}
                 />
             <p>Here are all the current details we have</p>
             <ul>
